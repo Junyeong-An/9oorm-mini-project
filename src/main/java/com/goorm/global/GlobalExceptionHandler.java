e1256f241089d7ef441f44052f1b0a12c7a04ef4
Junyeong-An <2117ab@gmail.com>
2024-10-04 06:14:36 +0900

    feat: 전역 예외 처리 추가

---
+++ src/main/java/com/goorm/global/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.goorm.global;
+
+import com.goorm.global.api.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiResponse<?>> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<>(ApiResponse.error(400, ex.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+}
